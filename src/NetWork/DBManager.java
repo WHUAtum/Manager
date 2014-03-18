4d1144327293b55091f62ed4d4075aa27a77ecab
sunatum <sunatum@qq.com>
2014-03-18 22:54:45 +0800

    Revert 83d1313..d128cf7
    This rolls back to commit 83d13135032f0f85df85011eaa9216588e619db5.

---
+++ src/NetWork/DBManager.java
@@ -0,0 +1,113 @@
+package NetWork;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBManager {
+	public String USER;
+	public String PASS;
+	public String DB_URL;
+	public Connection conn = null;
+	public Statement stmt = null;
+
+	public void DBManager(String username, String password, String URL) {
+		try {
+			USER = username;
+			PASS = password;
+			DB_URL = URL;
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	public void DBManager() {
+		try {
+			USER = "root";
+			PASS = "";
+			DB_URL = "jdbc:mysql://localhost:3306";
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	public boolean connectdb() {
+
+		try {
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connect to database successfully");
+			stmt = conn.createStatement();
+			return true;
+		} catch (SQLException se) {
+			// Handle errors for JDBC
+			se.printStackTrace();
+			return false;
+		} catch (Exception e) {
+			// Handle errors for Class.forName
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	
+
+	public void add(String valus, String table) {
+		String sql = "INSERT INTO " + table + "values(" + valus + ")";
+		try {
+			stmt.execute(sql);
+			System.out.println("register successful!");
+		} catch (Exception e) {
+			System.out.println("register error!");
+			e.printStackTrace();
+		}
+	}
+
+	public void del(String mac,String table) {
+		String sql="delete from "+table+" where mac="+mac;
+		try {
+			stmt.execute(sql);
+			System.out.println("unregister successful!");
+		} catch (Exception e) {
+			System.out.println("unregister error!");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void end() throws Exception{
+		
+		conn.close();
+		System.out.println("Goodbye!");
+	}
+
+	public boolean isexist(String mac, String table) {
+
+		String sql = "SELECT " + mac + " FROM " + table + " order by id";
+		try {
+			ResultSet rs = stmt.executeQuery(sql);
+			if(!rs.wasNull())
+				return true;
+			else return false;
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			return false;
+		}
+		
+	}
+
+	public void update() {
+		String sql;
+
+	}
+	public void addtable() {
+
+	}
+}
---
+++ src/NetWork/Manager.java
@@ -0,0 +1,55 @@
+package NetWork;
+
+import java.net.*;
+
+public class Manager {
+	static InetAddress Register(){
+		InetAddress address;
+		try{
+			ServerSocket Reg=new ServerSocket(18888);
+			Socket Regreceiver=Reg.accept();	
+			address= Regreceiver.getInetAddress();
+			Regreceiver.close();
+			return address;
+		}
+		catch(Exception e){
+			System.out.println("Register error!!");
+			return (InetAddress)new Object();
+		}
+		
+		
+	}
+	public static void main(String[] args){
+		
+        
+		try{
+			//connect the Unity3D Server
+			DBManager db=new DBManager();
+			System.out.print("Press enter if the Server ready!");		
+			System.in.read();
+			Socket ss=new Socket("192.168.1.38",17000); 
+	        System.out.println("Connect the Server successfully!");		
+	        if(!db.connectdb()){
+	        	 System.out.println("please check the database and press enter!");		
+	        	 System.in.read();
+	        }
+	        ServerSocket s=new ServerSocket(16000);
+	        //connect the devices
+	        while(true){
+				System.out.println("Waiting for Clients");
+					
+				Socket rs=s.accept();				
+				System.out.println("Client connected");
+			    //Create a new channel to for a device
+				new Channel(ss,rs,db).start();			
+			}
+		}
+		catch(Exception e){
+			System.out.println("Can not Connect the server!");
+			e.printStackTrace();
+		}
+	
+		
+	}
+	
+}
